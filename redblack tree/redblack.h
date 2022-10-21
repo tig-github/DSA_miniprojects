223d268c69b62434fc3846f02ae165d79265ed2b
Rayan Tighiouart <t.alex.2002@gmail.com>
2022-10-21 13:00:41 -0700

    more work on redblack tree

--- redblack tree/redblack.h
+++ redblack tree/redblack.h
@@ -6,7 +6,7 @@
 template <typename T>
 class RedBlackTree
 {
-private:
+public:
     struct Node
     { // parent pointer implementation
         T data;
@@ -16,24 +16,16 @@ private:
 
 public:
     using V = std::function<void(const T &)>;
+    Node *root;
+    int sz;
 
     RedBlackTree();
     RedBlackTree(const RedBlackTree<T> &tree);
-    ~RedBlackTree() noexcept
-    {
-    }
-    RedBlackTree(RedBlackTree<T> &&tree) noexcept
-    {
-    }
-    RedBlackTree<T> operator=(const RedBlackTree<T> &tree)
-    {
-    }
-    RedBlackTree<T> operator=(const RedBlackTree<T> &&tree) noexcept
-    {
-    }
-    int size() const noexcept
-    {
-    }
+    ~RedBlackTree() noexcept;
+    RedBlackTree(RedBlackTree<T> &&tree) noexcept;
+    RedBlackTree<T> operator=(const RedBlackTree<T> &tree);
+    RedBlackTree<T> operator=(const RedBlackTree<T> &&tree) noexcept;
+    int size() const noexcept;
     void add(const T value)
     {
     }
@@ -60,16 +52,75 @@ public:
     void postorder(V visit) const
     {
     }
+
+private:
+    void deleteTree(Node *tree);
+    RedBlackTree<T> copyTree(RedBlackTree<T> copy);
 };
 
 template <typename T>
 RedBlackTree<T>::RedBlackTree()
 {
+    root = new Node{null, null, null};
+    sz = 0;
 }
 
 template <typename T>
 RedBlackTree<T>::RedBlackTree(const RedBlackTree<T> &tree)
 {
+    root = copyTree(tree);
+    sz = 0;
 }
 
+template <typename T>
+RedBlackTree<T>::~RedBlackTree()
+{
+    deleteTree(root);
+}
+
+template <typename T>
+RedBlackTree<T>::RedBlackTree(RedBlackTree<T> &&tree) noexcept
+{
+    root = std::move(tree.root);
+    sz = std::move(tree.sz);
+}
+
+template <typename T>
+void RedBlackTree<T>::deleteTree(Node *tree)
+{
+    deleteTree(tree->left);
+    deleteTree(tree->right);
+    delete tree;
+}
+
+template <typename T>
+RedBlackTree<T> RedBlackTree<T>::copyTree(RedBlackTree<T> copy)
+{
+    Node *origin = new Node *{null, null, null};
+    origin->left = copyTree(copy.head->left);
+    origin->right = copyTree(copy.head->right);
+    return origin;
+}
+
+template <typename T>
+RedBlackTree<T> RedBlackTree<T>::operator=(const RedBlackTree<T> &tree)
+{
+    root = copyTree(tree.node);
+    sz = tree.sz;
+    return *this;
+}
+
+template <typename T>
+RedBlackTree<T> RedBlackTree<T>::operator=(const RedBlackTree<T> &&tree) noexcept
+{
+    root = std::move(tree.node);
+    sz = tree.sz;
+    return *this;
+}
+
+template <typename T>
+int RedBlackTree<T>::size() const noexcept
+{
+    return sz;
+}
 #endif
