d8510f22cb9f7034c2f43f4940c33bd21eb23264
Rayan Tighiouart <t.alex.2002@gmail.com>
2023-01-28 22:04:45 -0800

    added primitives

--- dcel/GeometricPrimitives.h
+++ dcel/GeometricPrimitives.h
@@ -2,10 +2,26 @@
 #define GeometricPrimitives
 
 
+template <typename numberType>
+struct Line { // Ax + By = C
+    numberType A;
+    numberType B;
+    numberType C;
+};
 
-int computeLineIntersection(int x1, int x2, int y1, int y2);
+// creates a standard form linear equation given a line
+template <typename num>
+Line<numberType> generateLine(num x1, num x2, num y1, num y2){
+    
+}
 
 
+// computes the intersection of 2 lines (x1,y1);(x2,y2) and returns -1 if parallel
+template <typename num>
+num computeLineIntersection(num x1, num x2, num y1, num y2){
+
+
+}
 
 
 
